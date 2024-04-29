8fa88bc488c7c99cf53b12de20729a38765cb437
Kevin Emiro Ospinal Mena <111004796+Keospinal@users.noreply.github.com>
2024-04-28 21:10:23 -0500

    Proyecto
    Proyecto completo

---
+++ Lab-DespliegueCloud/src/main/java/com/example/models/ProductoDTO.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.models;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Kevin Emiro Ospinal
+ */
+public class ProductoDTO {
+    
+    
+    public ProductoDTO() {
+    }
+    
+    private String nombre;
+    private String marca;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    
+    
+}
